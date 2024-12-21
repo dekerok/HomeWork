0200d06ce95738ff2ac56c84d8c8551397cf4949
Deineka Alex <dekerok@mail.ru>
2024-12-22 06:40:21 +0700

    init

--- src/main.py
+++ src/main.py
@@ -1,18 +1,17 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
 
-    '''вызов функций по домашке 9.1'''
+    """вызов функций по домашке 9.1"""
     user_card_number = input("Введите номер карты: ")
     print(get_mask_card_number(user_card_number))
 
     user_account_number = input("Введите номер счета: ")
     print(get_mask_account(user_account_number))
 
-    '''вызов функций по домашке 9.2'''
+    """вызов функций по домашке 9.2"""
     user_bank_input = input("Введите номер счета или карты: ")
     print(mask_account_card(user_bank_input))
 
     print(get_date("2024-03-11T02:26:18.671407"))
-
--- src/widget.py
+++ src/widget.py
@@ -1,20 +1,22 @@
-from src.masks import get_mask_card_number, get_mask_account
 from typing import Union
 
+from src.masks import get_mask_account, get_mask_card_number
 
-def mask_account_card(user_bank_input:Union[str]) -> Union[str]:
-    '''Функция принимает от пользователя информацию
-    определяет номер или счет'''
+
+def mask_account_card(user_bank_input: Union[str]) -> Union[str]:
+    """Функция принимает от пользователя информацию
+    определяет номер или счет"""
     card_or_account = user_bank_input.split()[-1]
-    if len(card_or_account) <=16:
+    if len(card_or_account) <= 16:
         return get_mask_card_number(user_bank_input)
     else:
         return get_mask_account(user_bank_input)
 
-def get_date(date_info:Union[str]) -> Union[str]:
+
+def get_date(date_info: Union[str]) -> Union[str]:
     '''Функция которая принимает на вход строку
-     и отдает корректный результат в формате
-"ДД.ММ.ГГГГ"'''
-    only_date = date_info.split('T')[0]
-    year, month, day = only_date.split('-')
+         и отдает корректный результат в формате
+    "ДД.ММ.ГГГГ"'''
+    only_date = date_info.split("T")[0]
+    year, month, day = only_date.split("-")
     return f"{day}.{month}.{year}"
